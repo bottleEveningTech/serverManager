6620e6866cfd8b2077a2ece0532ef3338013a867
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-12 22:03:37 +0530

    ElementRef

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, input, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, HostBinding, HostListener, inject, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,17 +7,20 @@ import { Component, HostBinding, HostListener, input, Input, ViewEncapsulation }
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, //emulated is default,
-  // host:{
-  //   class: 'control',
-  //   '(click)':'onClick()'
-  // }
+  host:{
+    class: 'control',
+    '(click)':'onClick()'
+  }
 })
 export class ControlComponent {
 
   // @HostBinding('class') className = 'control';
   // @HostListener('click') 
+
+  private el = inject(ElementRef);
   onClick(){
     console.log('clicked');
+    console.log(this.el);
   };
   label = input.required<string>();
 
